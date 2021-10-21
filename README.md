3351c4f3148de4f667dba1fb8ae0621ef65a09dd
Sopelka <evverything@yandex.ru>
2021-10-21 22:39:00 +0300

    structure refactoring - init commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cozySite
+# Portfolio
 
---
+++ src/pages/index/scripts/index.js
@@ -0,0 +1,24 @@
+import '../../../styles/index.scss';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+
+$(document).ready(() => {
+    $('.owl-carousel').owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dotsEach:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
