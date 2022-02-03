fee7e146317ae52ed9029c40e4c469652f22733c
Valentina Klepikova <skle.vale@gmail.com>
2022-02-03 22:32:40 +0300

    modal added, refactoring required

--- src/pages/index/scripts/index.js
+++ src/pages/index/scripts/index.js
@@ -1,19 +1,31 @@
 import '../../../styles/index.scss';
 
 
-// (function () {
-//     const picArr = ['11.png', '10.png','9.png','8.png','7.png','6.png','5.png','4.png','3.png','2.png','1.png'];
-//     const container = document.getElementById('hero-pic__id');
+(function () {
+    const picArr = ['11.png', '10.png','9.png','8.png','7.png','6.png','5.png','4.png','3.png','2.png','1.png'];
+    const container = document.getElementById('hero-pic__id');
 
-//     let i = 0;
+    let i = 0;
 
-//     function showPic(){
-//         container.setAttribute('src', `img/heroSlider/${picArr[i]}`);
-//         i++;
-//         if (i >= 11) {
-//             i = 0;
-//         }
-//     };
+    function showPic(){
+        container.setAttribute('src', `img/heroSlider/${picArr[i]}`);
+        i++;
+        if (i >= 11) {
+            i = 0;
+        }
+    };
 
-//     setInterval(showPic, 2000);
-// })();
+    setInterval(showPic, 2000);
+})();
+
+(function () {
+    document.getElementById('modal').style.display = 'none';
+    
+    document.getElementById('close-btn_id').addEventListener('click', (event)=>{
+        document.getElementById('modal').style.display = 'none';
+    });
+
+    document.getElementById('contacts_id').addEventListener('click', (event)=>{
+        document.getElementById('modal').style.display = 'block';
+    });
+})();
